2161ca3c6d269c0a6efdabb7f6aeb9dff9a45363
Saur-Deshmukh <saur90044@gmail.com>
2025-04-27 07:59:42 +0530

    logo added

--- Frontend/components/navbar.tsx
+++ Frontend/components/navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
-          <LockKeyhole className="h-6 w-6 text-teal-400" />
+          <img src="/logo.png" className="h-9 w-9"/>
           <span className="font-bold text-xl">FortiPhrase</span>
         </Link>
 
